2a2bcc4decfb9edff4473764385bb54acd2d754b
Roberta Nascimento <roberta.nask@gmail.com>
2025-07-08 13:25:41 -0300

    feat: introduction to for loop

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
+        int n = sc.nextInt();
         int soma = 0;
 
-        while (x != 0) {
-            soma += x;
-            x = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            soma = soma + x;
+            System.out.println(soma);
         }
 
-        System.out.println(soma);
         sc.close();
     }
 }
